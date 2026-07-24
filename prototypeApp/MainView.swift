763a102e016e233284888192a0a51d10c70fa600
Anubhav <programmingwithanubhav@gmail.com>
2026-07-24 22:39:15 +0530

    inital updates

---
+++ prototypeApp/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  Main.swift
+//  prototypeApp
+//
+//  Created by Anubhav Dubey on 24/07/26.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
